985710ec298cc26fdcfb0be804f665ec82b6aa06
jonchiu94 <jchiu84@my.bcit.ca>
2018-12-07 23:28:43 -0800

    adding changes to heap

--- heap.hpp
+++ heap.hpp
@@ -8,7 +8,7 @@ using namespace std;
 template <typename T>
 class heap {
     private:
-        // vector to store heap element
+        // vector to store heap elements
         vector<T> heap_vector;
 
         //Returns the location of parent
